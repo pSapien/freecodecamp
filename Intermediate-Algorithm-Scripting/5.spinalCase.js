11ccea29bb2d14d1129e6ce3240b6995b1bdae50
Prateek Thapa <prateekthapa26@gmail.com>
2020-03-20 15:41:08 +0545

    spinalCase

---
+++ Intermediate-Algorithm-Scripting/5.spinalCase.js
@@ -0,0 +1,27 @@
+/** Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes. */
+
+const isUpper = str => str.toUpperCase() === str;
+const isAtoZ = str => str.match(/^[A-Za-z]+$/);
+
+function spinalCase(str) {
+  let [first, ...rest] = str;
+  let res = first.toLowerCase();
+
+  for (let i = 0; i < rest.length; i++) {
+    const char = rest[i];
+    if (char === '-') res += char;
+    if (!isAtoZ(char)) continue;
+
+    if (isUpper(char) || rest[i - 1] === ' ') {
+      res += `-${char.toLowerCase()}`;
+      continue;
+    }
+    res += char;
+  }
+
+  return res;
+}
+
+console.log(spinalCase("This Is Spinal Tap"));
+console.log(spinalCase("AllThe-small Things"));
+console.log(spinalCase("Teletubbies say Eh-oh"));
