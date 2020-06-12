0ad423eceabc20dec99c6fa005d98cec45fa9a58
Prateek Thapa <prateekthapa26@gmail.com>
2020-06-12 20:49:11 +0545

    euler - add link and question

--- coding-interview-preparation/euler/1.multiplesOf3and5.js
+++ coding-interview-preparation/euler/1.multiplesOf3and5.js
@@ -1,3 +1,13 @@
+/**
+ * 
+ * @url https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-1-multiples-of-3-and-5
+ * Project Euler: Problem 1: Multiples of 3 and 5Passed
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+ */
+
+
 const isMultipleOf3or5 = v => v % 3 === 0 || v % 5 === 0;
 
 function multiplesOf3and5(number) {
@@ -10,5 +20,3 @@ function multiplesOf3and5(number) {
   return sum;
 }
 
-console.log(multiplesOf3and5(10));
-console.log(multiplesOf3and5(0));
