a5d10f2d09e11ca00b9c432106c0a865e1726359
Prateek Thapa <prateekthapa26@gmail.com>
2020-06-25 11:56:24 +0545

    ds - stack

---
+++ coding-interview-preparation/data-structures/Stack.js
@@ -0,0 +1,26 @@
+class Stack {
+  constructor() {
+    this.collection= [];
+  }
+
+  push(v) {
+    this.collection.push(v);
+  }
+
+  pop() {
+    return this.collection.pop();
+  }
+
+  peek() {
+    if (this.collection.length === 0) return undefined;
+    return this.collection[0];
+  }
+
+  isEmpty() {
+    return !Boolean(this.collection.length);
+  }
+
+  clear() {
+    this.collection.length = 0;
+  }
+}
