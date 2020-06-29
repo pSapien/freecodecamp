38c21f2f01f010500385ccca2b6cc3f9819a5054
Prateek Thapa <prateekthapa26@gmail.com>
2020-06-29 18:44:57 +0545

    set - union, intersection, difference, isSubsetOf

--- coding-interview-preparation/data-structures/Set.js
+++ coding-interview-preparation/data-structures/Set.js
@@ -14,14 +14,14 @@ class Set {
 
   add(element) {
     if (this.has(element)) return false;
-    
+
     this.dictionary[element] = true;
     this.length++;
     return true;
   }
-   
+
   remove(element) {
-    if (this.has(element)) { 
+    if (this.has(element)) {
       delete this.dictionary[element];
       this.length--;
       return true;
@@ -32,5 +32,44 @@ class Set {
 
   size() {
     return this.length;
+  }
+
+  union(setB) {
+    const union = new Set();
+    this.values().forEach(elemA => union.add(elemA));
+    setB.values().forEach(elemB => union.add(elemB));
+    return union;
+  }
+
+  intersection(setB) {
+    const intersection = new Set();
+
+    const isLargerSet = this.dictionary.length > setB.length;
+    const largeSet = isLargerSet ? this : setB;
+    const smallSet = isLargerSet ? setB : this;
+
+    smallSet.values().forEach(elemA => {
+      if (largeSet.has(elemA)) intersection.add(elemA);
+    });
+
+    return intersection;
+  }
+
+  difference(setB) {
+    const diff = new Set();
+
+    const isLargerSet = this.dictionary.length > setB.length;
+    const largeSet = isLargerSet ? this : setB;
+    const smallSet = isLargerSet ? setB : this;
+
+    smallSet.values().forEach(elemA => {
+      if (!largeSet.has(elemA)) diff.add(elemA);
+    });
+
+    return diff;
+  }
+
+  isSubsetOf(setB) {
+    return this.values().every(elemA => setB.has(elemA));
   }
 }
